e404ae6d3cf5a350f795463e90f8eb6950643b31
hsyoodev <hsyoodev@gmail.com>
2024-01-10 17:28:22 +0900

    CDN 방식에서 CRA 방식으로

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>WEB</h1>
+      World Wide Web!
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="1.html">HTML</a>
+        </li>
+        <li>
+          <a href="2.html">CSS</a>
+        </li>
+        <li>
+          <a href="3.html">JavaScript</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
+      <Header />
+      <Nav />
     </div>
   );
 }
