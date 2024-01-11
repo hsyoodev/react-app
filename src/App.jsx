0fdea52e7303eb84a56a68d5c668f4da07a0c854
hsyoodev <hsyoodev@gmail.com>
2024-01-11 16:15:56 +0900

    연습문제

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import CommentList from './CommentList';
+import Header from './components/Header';
+import Section from './components/Section';
+import Article from './components/Article';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <CommentList />
+      <Header />
+      <Section />
+      <Article />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>야구게임 방식</h1>
+    </header>
+  );
+}
+
+Header.propTypes = {};
+
+export default Header;
