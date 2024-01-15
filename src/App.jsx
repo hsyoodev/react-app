b2f6420c134d978aea4d5c46ed35decd8d60b3a1
hsyoodev <hsyoodev@gmail.com>
2024-01-15 16:20:58 +0900

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import MinutesToHours from './components/MinutesToHours';
+import { useState, useMemo, useRef } from 'react';
 
-function App() {
+function Counter() {
+  let value = useRef(0);
+  const [count, setCount] = useState(0);
+
+  const onClick = () => {
+    value.current++;
+    console.log(`value: ${value.current}`);
+    setCount(count + 1);
+  };
+  const factorial = (n) => {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else {
+      return factorial(n - 1) * n;
+    }
+  };
+  const memo = useMemo(() => {
+    console.log('factorial 연산');
+    let result = factorial(count);
+    return result;
+  }, [count]);
+
   return (
-    <>
-      <MinutesToHours />
-    </>
+    <div>
+      <h1>{count}</h1>
+      <h1>팩토리얼 연산 결과: {memo}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
   );
 }
 
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
 export default App;
