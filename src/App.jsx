e4ca4bb6e61d760ab24ba43c080a0898c547d6ef
hsyoodev <hsyoodev@gmail.com>
2024-01-17 15:00:02 +0900

    카카오 지도

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,110 @@
 import { useEffect, useState } from 'react';
-import Area from './components/Area';
-import Clock from './components/Clock';
-import Weather from './components/Weather';
 
-function App() {
-  const [area, setArea] = useState({});
-  const [weather, setWeather] = useState({});
+function NameForm({ setName }) {
+  const [value, setValue] = useState('');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    alert(`입력한 이름: ${value}`);
+    setName(value);
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        이름:
+        <input type="text" value={value} onChange={handleChange} />
+      </label>
+      <button type="submit">제출</button>
+    </form>
+  );
+}
+
+function TextAreaForm({ setRequest }) {
+  const [value, setValue] = useState('요청사항을 입력하세요.');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    alert(`입력한 요청사항: ${value}`);
+    setRequest(value);
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        요청사항:
+        <textarea value={value} onChange={handleChange} />
+      </label>
+      <button type="submit">제출</button>
+    </form>
+  );
+}
+
+function SelectForm({ setSelect }) {
+  const [value, setValue] = useState('grape');
+  const handleChange = (event) => setValue(event.target.value);
+  const handleSubmit = (event) => {
+    alert(`선택한 과일: ${value}`);
+    setSelect(value);
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        과일을 선택하세요:
+        <select value={value} onChange={handleChange}>
+          <option value="apple">사과</option>{' '}
+          <option value="banana">바나나</option>
+          <option value="grape">포도</option>{' '}
+          <option value="watermelon">수박</option>
+        </select>
+      </label>
+      <button type="submit">제출</button>
+    </form>
+  );
+}
+
+function Map({ latlon }) {
+  const kakao = window.kakao;
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(latlon.lat, latlon.lon),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, [latlon]);
 
-      get(lat, lon);
+  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
+}
+
+function App() {
+  const [name, setName] = useState('');
+  const [request, setRequest] = useState('');
+  const [select, setSelect] = useState('');
+  const [latlon, setLatLon] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
+      console.log(lat, lon);
+      setLatLon({ lat, lon });
     });
-
-    async function get(lat, lon) {
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=6edee3c2aa182bc44d18ccb204c98a31`
-      );
-      const res = await data.json();
-      const name = res.name;
-      setArea({ name, lat, lon });
-
-      const icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
-      const temp = res.main.temp;
-      const speed = res.wind.speed;
-      const main = res.weather[0].main;
-      setWeather({ icon, temp, speed, main });
-    }
   }, []);
 
   return (
     <>
-      <Clock />
-      <Area data={area} />
-      <Weather data={weather} />
+      <NameForm setName={setName} />
+      <TextAreaForm setRequest={setRequest} />
+      <SelectForm setSelect={setSelect} />
+      <hr />
+      이름 : {name}, 요청사항 : {request}, 과일 : {select}
+      <Map latlon={latlon} />
     </>
   );
 }
